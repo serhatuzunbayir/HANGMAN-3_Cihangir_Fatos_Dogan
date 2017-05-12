32edda63a0e31c1cc0beeada707a1c71dabcdc06
dogandirik <dogan_dirik@hotmail.com>
2017-05-12 12:25:54 +0300

    Version 3 of Hangman
    Added comments and replaced searchLetter function

--- Hangman/src/Hangman/Game.java
+++ Hangman/src/Hangman/Game.java
@@ -1,60 +1,60 @@
 package Hangman;
 
-import java.util.Scanner;
+import java.util.Scanner;   // import scanner library for scanner use 
 
 public class Game {
 
 	public static void main(String[] args) {
-		int resultAgain=0;
-		Scanner scnlg = new Scanner(System.in);
-		Scanner scn = new Scanner(System.in);
+		int resultAgain=0;                     //Define resultAgain
+		Scanner scnlg = new Scanner(System.in);	 // Create scnlg from scanner for multilanguage. 
+		Scanner scn = new Scanner(System.in);	 // Create  scn from scanner for letters.
 		do
 		{
-		System.out.println("Select Directory Language :");
+		System.out.println("Select Directory Language :"); // Multiple language option
 		System.out.println("1- English Directory :");
 		System.out.println("2- Turkish Directory :");
 		
 		Word searchWord;
-		int result = scnlg.nextInt();
+		int result = scnlg.nextInt();  //The user's dictionary selection is taken and put into the result
 		
 		if(result==1)
 		{
-			searchWord = new Word(1);
+			searchWord = new Word(1);  //English dictionary dictionary income
 		}
 		else if(result==2)
 		{
-			searchWord = new Word(2);
+			searchWord = new Word(2);  //Turkish dictionary dictionary income
 		}
 		else
 		{
-			searchWord = new Word(1);
+			searchWord = new Word(1);  // English dictionary dictionary income as default
 		}
 		
 		
-		String visibleWord = searchWord.secretWord;
+		String visibleWord = searchWord.secretWord;   
 		System.out.println(" ");
-		System.out.println("Your secret word's length is " + visibleWord.length());
+		System.out.println("Your secret word's length is " + visibleWord.length()); //Secret word of length is printed on the screen as number 
 		
-		while(searchWord.counter<=4)
+		while(searchWord.counter<=4)  //while runs until searchword counter is 4
 		{
 			System.out.println("Please enter a letter");
-			char newLetter = scn.next().charAt(0);
+			char newLetter = scn.next().charAt(0);   
 			System.out.println(searchWord.searchLetter(newLetter));
 			if (searchWord.truecounter == visibleWord.length()) 
 			{
-				System.out.println("WIN");			
+				System.out.println("WIN");  // if truecounter is equal to visible word of length user is win
 				break;
 			}
 		}		
 
-		System.out.println("Do you wanna1 play again ?");
+		System.out.println("Do you wanna play again ?"); //Ask the user
 		System.out.println("1 - Yes");
 		System.out.println("2- No");
-		resultAgain = scnlg.nextInt();
+		resultAgain = scnlg.nextInt(); //Resultagain is taken income number
 		
 
 		
-		}while(resultAgain==1);
+		}while(resultAgain==1); 
 		scn.close();
 		scnlg.close();	
 	}
--- Hangman/src/Hangman/Word.java
+++ Hangman/src/Hangman/Word.java
@@ -1,38 +1,40 @@
 package Hangman;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.File;         //import input-output library for 
+import java.util.ArrayList; //import ArrayList library
+import java.util.Random;   //import Random library for random selection
+import java.util.Scanner; //import Scanner library for scanner use
 
 public class Word {
-	Word(int lg)
+	Word(int lg) 
 	{
 		getWordList(lg);
-		assignWord();
-		createNullWord();
-		myWord = new StringBuilder(createNullWord());
+		assignWord();  // Random word assign
+		createNullWord(); //Define null word
+		myWord = new StringBuilder(createNullWord()); // Defined null word is put with stringbuilder
 	}
-	protected StringBuilder myWord;
-	public String secretWord;
-	public int counter =0;
+	//--Define classes--//
+	protected StringBuilder myWord;  
+	public String secretWord;       
+	public int counter =0;         
 	public int truecounter =0;
 	protected ArrayList<String> WordList = new ArrayList<String>();
 	protected Scanner wordScanner = null;
 	protected ArrayList<Character> letterList = new ArrayList<Character>();
 	protected String createNullWord()
 	{
-		String displayedResult = "";
-		for(int i=0; i<secretWord.length(); i++)
+		String displayedResult = ""; 
+		for(int i=0; i<secretWord.length(); i++) // For loop runs until the length of the secret word
 		{
-			displayedResult = displayedResult+"_";
+			displayedResult = displayedResult+"_";  //The secret word is written as '-' on the screen.
 		}
 		return displayedResult;
 	}
-	protected void getWordList(int lg)
+	protected void getWordList(int lg)  // getwordList class is taken int parameter(option)
 	{
 		try
 		{
+			//--Swtich reads the file to according the incoming lg result --//
 			switch(lg)
 			{
 			case 1: wordScanner = new Scanner(new File("C:\\Users\\Dogan\\Desktop\\SE318\\HANGMAN-3_Cihangir_Fatos_Dogan\\Hangman\\src\\Hangman\\Words.txt"));
@@ -44,63 +46,65 @@ public class Word {
 			}
 				
 			while(wordScanner.hasNextLine()) {
-				String[] words = wordScanner.nextLine().split(",");
-				for(int i=0;i<words.length;i++) 
+				String[] words = wordScanner.nextLine().split(",");  //Separated by comma
+				for(int i=0;i<words.length;i++)                     //For loop runs until the length of the word
 				{
-					String temp = words[i].trim();
-					WordList.add(temp);
+					String temp = words[i].trim();       //Trim is used to get rid of spaces and put into temp
+					WordList.add(temp);                 // added to wordlist
 				}
 			}	
 		}
 		catch(Exception e)
 		{
-			System.out.println("Your Error : " + e.getMessage());
+			System.out.println("Your Error : " + e.getMessage()); //Message is given in case of exception
 		}
 	}
 	protected void assignWord()
 	{
-		Random random = new Random();
-		int number =random.nextInt(WordList.size());
-		secretWord = WordList.get(number);
+		Random random = new Random();  //Random is created.
+		int number =random.nextInt(WordList.size());  //Wordlist of length is taken as randomly and put to number
+		secretWord = WordList.get(number);	     // The secret word is taken from the wordlist with the incoming number
 	}
 	public void addWord(String word){
-		WordList.add(word);
+		WordList.add(word);  //Added word to wordlist
 	}
-	protected void displayWord(){
+	protected void displayWord(){    //Display word on the screen 
 		int i = 0;
-		for (String item : WordList) {
-			System.out.println((i++)+" "+item);
+		for (String item : WordList) {         
+			System.out.println((i++)+" "+item);   
 		}
 	}
-	public void modifyWord(int Choose,String result){
-		WordList.set(Choose, result);
+	public void modifyWord(int Choose,String result){   // Modifyword is taken 2 parameter
+		WordList.set(Choose, result);         //Set the Wordlist
 	}
-	public String searchLetter(char letter)
+	public String searchLetter(char letter)  //take parameter(leter)
 	{	
-		if(letterList.size()!=0)
+		if(letterList.size()!=0)               //letterlist size is not equal zero
 		{
-			for(int k=0; k<letterList.size();k++)
+			for(int k=0; k<letterList.size();k++)   // for loop runs until letterlist of size
 			{
-				if(letterList.get(k)==letter)
+				if(letterList.get(k)==letter)  //letterlist's k is equal to incoming parameter(letter)
 				{
-					return myWord.toString();
+					System.out.println("This letter has already been entered. Please enter a new letter.");
+					return myWord.toString();  //The myword is converted to string and return
 				}
 			}
 		}
-		int temp = truecounter;
-		for(int i=0; i<secretWord.length(); i++)
+		int temp = truecounter;        //keep true answers to temp
+		for(int i=0; i<secretWord.length(); i++)    //for loop runs until secretword of length
 		{
-			if(letter==secretWord.charAt(i))
+			if(letter==secretWord.charAt(i))   //letter is equal secretword's character
 			{						
-				myWord.setCharAt(i, letter);
+				myWord.setCharAt(i, letter); //Myword is set 
 				truecounter += 1;
-				letterList.add(letter);
+				letterList.add(letter);	   //letter is added to letterlist
 			}
 		}
-		if(temp == truecounter)
+		if(temp == truecounter) 
 		{
 			counter +=1;
-			letterList.add(letter);
+			letterList.add(letter); //letter is added to letterlist
+			//--The message is given to the screen in every wrong answer according to the incoming counter
 			switch(counter)
 			{
 				case 1: System.out.println("The head was cut!"); break;
@@ -110,6 +114,6 @@ public class Word {
 				case 5: System.out.println("The legs were cut! GAME OVER !!!");break; 
 			}
 		}
-		return myWord.toString();
+		return myWord.toString(); // return myword
 	}
 }
